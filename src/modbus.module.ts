e572e5c2462d68035f2c85b21c2852794f7cea22
Krzysztof Orkisz <krzysztof.orkisz@softwareone.com>
2021-11-07 22:29:21 +0100

    export dynamic providers

--- src/modbus.module.ts
+++ src/modbus.module.ts
@@ -1,23 +1,24 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { Configuration, SerialDeviceConfiguration } from './config/configuration';
-import { ModbusReadCoilsQueryHandler } from './modbus-read-coils-query-handler';
 import { ModbusSerialService } from './modbus-serial.service';
 
 @Module({})
 export class ModbusModule {
   static register(config: Configuration): DynamicModule {
+    const dynamicProviders = config.serialDevices.map((serialConfig: SerialDeviceConfiguration) => {
+      return {
+        provide: `serial-${serialConfig.id}`,
+        useFactory: () => ModbusSerialService.create(serialConfig),
+      };
+    });
     return {
       module: ModbusModule,
       providers: [
-        ...config.serialDevices.map((serialConfig: SerialDeviceConfiguration) => {
-            return {
-              provide: `serial-${serialConfig.id}`,
-              useFactory: () => ModbusSerialService.create(serialConfig),
-            };
-          },
-          ModbusReadCoilsQueryHandler,
-        ),
+        ...dynamicProviders,
       ],
+      exports: [
+        ...dynamicProviders
+      ]
     };
   }
 }
